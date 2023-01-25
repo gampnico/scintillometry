4013be2591cd7786ceced4db7fba4579c293adeb
gampnico <45390064+gampnico@users.noreply.github.com>
2023-01-25 18:03:21 +0100

    feat!: add user arguments for importing .mnd files
    Adds user arguments to cli interface for selecting input file,
    converting timezone, showing the help message, running in verbose mode,
    and a "dry-run" switch for future implentations of the footprint
    climatology.
    
    Package can be run directly via main.py:
        src/scintillometry/main.py [-h] [-i <input_data>] [-d] [...] [-v]
    
    Adds required arguments:
        -i, --input <path>: Path to raw BLS450 data.
    
    Adds optional switches:
        -h, --help: Show this help message and exit.
        -z, --dry-run: Dry run of model.
        -v, --verbose: Verbose mode.
    
    Adds optional arguments:
        -t, --timezone <str>: Convert to local timezone. Default "CET".
    
    Refs: ST-5, ST-26, ST-31, ST-32, ST-33

---
+++ src/scintillometry/main.py
@@ -0,0 +1,116 @@
+"""Copyright 2023 Nicolas Gampierakis.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=====
+
+Analyse data & 2D flux footprints from Scintec's BLS scintillometers.
+
+usage: src/main.py [-h] [-i <input_data>] [-d] [...] [-v]
+
+Options and arguments (and corresponding environment variables):
+
+Required arguments:
+-i, --input <path>: Path to raw BLS450 data.
+
+Optional switches:
+-h, --help: Show this help message and exit.
+-z, --dry-run: Dry run of model.
+-v, --verbose: Verbose mode.
+
+Optional arguments:
+-t, --timezone <str>: Convert to local timezone. Default "CET".
+"""
+
+import argparse
+
+import scintillometry.wrangler.data_parser
+
+
+def user_argumentation():
+    """Parses user arguments when run as main.
+
+    Required arguments:
+        -i, --input <path>: Set path to input data file.
+
+    Optional switches:
+        -h, --help: Show this help message and exit.
+        -z, --dry-run: Dry run of model.
+        -v, --verbose: Verbose mode.
+
+    Optional arguments:
+        -t, --timezone <str>: Convert to local timezone. Default "CET".
+
+    Returns:
+        argparse.Namespace: Namespace of user arguments.
+    """
+
+    tagline = "Analyse data & 2D flux footprints from Scintec's BLS scintillometers."
+    parser = argparse.ArgumentParser(description=tagline)
+    # Required
+    parser.add_argument(
+        "-i",
+        "--input",
+        default=None,
+        dest="input",
+        type=str,
+        metavar="<path>",
+        required=True,
+        help="Path to raw BLS450 data",
+    )
+    # Switches
+    parser.add_argument(
+        "-z",
+        "--dry-run",
+        action="store_true",
+        default=None,
+        dest="dry_run",
+        help="Dry run",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        default=False,
+        dest="verbosity",
+        help="Verbose mode",
+    )
+    # Parameters
+    parser.add_argument(
+        "-t",
+        "--timezone",
+        dest="timezone",
+        metavar="<str>",
+        type=str,
+        required=False,
+        default="CET",
+        help="Convert to local timezone. Default 'CET'",
+    )
+    arguments = parser.parse_args()
+
+    return arguments
+
+
+def main():
+    args = user_argumentation()
+    user_input_file = args.input
+
+    # Import raw BLS450 data
+    input_data = scintillometry.wrangler.data_parser.parse_scintillometer(
+        file_path=user_input_file, timezone=args.timezone
+    )
+    print(input_data.head())
+
+
+if __name__ == "__main__":
+    main()
