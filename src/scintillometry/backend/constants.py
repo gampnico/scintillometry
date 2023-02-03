7e6c2c38edc1c4f1118aca608b8320d287308040
gampnico <45390064+gampnico@users.noreply.github.com>
2023-02-03 15:50:03 +0100

    feat(backend)!: implement constants as class
    The class AtmosConstants initialises with various preset constants, to
    avoid mismatching constants between functions.
    
    Refs: ST-3, ST-6, ST-41

---
+++ src/scintillometry/backend/constants.py
@@ -0,0 +1,119 @@
+"""Copyright 2019-2023 Helen Ward, Nicolas Gampierakis, Josef Zink.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=====
+
+Defines classes and functions for determining constants.
+"""
+
+
+class AtmosConstants(object):
+    """Atmospheric constants.
+
+    There seems to be a contradiction between the values for |a_11|
+    in the Scintillometer Theory Manual 1.05 (Chapter 1.1.5), and the
+    BLS Manual 1.49 (A.2 Eq.6). The Scintillometer Theory Manual is more
+    recent.
+
+    Attributes:
+        alpha11 (dict): |Cn2| measurement coefficients |a_11| for each
+            BLS type, [|m^(7/3)|].
+        alpha12 (dict): |Cn2| measurement coefficients |a_12| for each
+            BLS type.
+        lamda (float): BLS wavelength, λ [nm].
+        lamda_error (float): BLS wavelength error, [nm].
+        m1_opt (float): Needed for AT and Aq, from Owens (1967).
+        m2_opt (float): Needed for AT and Aq, from Owens (1967).
+
+        at_opt (float): AT coefficient for 880 nm & typical atmospheric
+            conditions.
+        aq_opt (float): Aq coefficient for 880 nm & typical atmospheric
+            conditions.
+
+        kelvin (float): 0°C in kelvins.
+        r_dry (float): Specific gas constant for dry air,
+            |R_dry| [J |K^-1| |kg^-1|].
+        r_vapour (float): Specific gas contstant for water vapour,
+            |R_v| [J |K^-1| |kg^-1|].
+        ratio_rmm (float): Ratio of molecular masses of water vapour and
+            dry air.
+        cp (float): Specific heat capacity of air at constant pressure,
+            |c_p| [J |K^-1| |kg^-1|].
+        latent_vapour (float): Latent heat of vapourisation at
+            20°C [J |kg^-1|].
+        rho (float): Density of air at STP, ρ [kg |m^-3|].
+        k (float): von Kármán's constant.
+        g (float): Gravitational acceleration [|ms^-2|].
+
+    .. |K^-1| replace:: K :sup:`-1`
+    .. |kg^-1| replace:: kg :sup:`-1`
+    .. |m^(7/3)| replace:: m :sup:`7/3`
+    .. |m^-3| replace:: m :sup:`-3`
+    .. |ms^-2| replace:: ms :sup:`-2`
+    .. |10^-5| replace:: 10 :sup:`-5`
+    .. |a_11| replace:: α :sub:`11`
+    .. |a_12| replace:: α :sub:`12`
+    .. |R_dry| replace:: R :sub:`dry`
+    .. |R_v| replace:: R :sub:`v`
+    .. |c_p| replace:: c :sub:`p`
+    .. |z_eff| replace:: z :sub:`eff`
+    .. |z_mean| replace:: :math:`\\bar{z}`
+    .. |Cn2| replace:: C :sub:`n`:sup:`2`
+    .. |CT2| replace:: C :sub:`T`:sup:`2`
+    """
+
+    def __init__(self):
+        super().__init__()
+
+        self.alpha11 = {
+            "450": 4.9491e-2,  # BLS450, 4.48 * D^(7/3), Theory manual 1.1.5
+            "900": 4.9491e-2,  # BLS900, 4.48 * D^(7/3), Theory manual 1.1.5
+            "2000": 1.9775e-1,  # BLS2000 4.48 * D^(7/3), Theory manual 1.1.5
+            "450_pra": 1.0964e-2,  # with path reduction aperture
+            "900_pra": 1.0964e-2,
+            "2000_pra": 4.5618e-2,
+            "450_alt": 5.5339e-2,  # BLS Manual A.2 Eq.6
+            "900_alt": 5.5339e-2,
+            "2000_alt": 1.9372e-1,
+        }
+        self.alpha12 = {  # N/A for BLS 450
+            "900": 0.50175,
+            "2000": 1.4768,
+            "900_pra": 0.86308,  # with path reduction aperture
+            "2000_pra": 2.2726,
+        }
+
+        self.lamda = 880e-9  # Scintillometer Theory Manual recommends 880 nm
+        self.lamda_error = 20e-9
+
+        # From Ward et al., (2012)
+        self.at_opt = -270e-6  # AT coefficient for 880 nm & typical atmos conditions
+        self.aq_opt = -0.685e-6  # Aq coefficient for 880 nm & typical atmos conditions
+
+        # Physical constants
+        self.kelvin = 273.15
+        self.r_dry = 287.04
+        self.r_vapour = 461.5
+        self.ratio_rmm = 0.622
+        self.cp = 1004.67
+        self.latent_vapour = 2.45e6
+        self.rho = 1.225
+        self.k = 0.4
+        self.g = 9.81
+
+    def get(self, constant_name: str):
+        return getattr(self, constant_name)
+
+    def overwrite(self, constant_name: str, value: float):
+        return setattr(self, constant_name, value)
---
+++ tests/test_backend_constants.py
@@ -0,0 +1,70 @@
+"""Copyright 2023 Nicolas Gampierakis.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=====
+
+Tests path weighting module.
+"""
+
+import pytest
+
+import scintillometry.backend.constants
+
+
+class TestBackendConstants:
+    """Test class for AtmosConstants class."""
+
+    @pytest.mark.dependency(name="TestBackendConstants::test_init", scope="session")
+    def test_constants_init(self):
+        test_class = scintillometry.backend.constants.AtmosConstants()
+        attributes_dict = test_class.__dict__
+        for constant in attributes_dict.values():
+            assert isinstance(constant, (float, dict))
+            if isinstance(constant, dict):
+                assert all(isinstance(value, float) for value in constant.values())
+
+    @pytest.mark.dependency(name="TestBackendConstants::test_get_error")
+    def test_get_error(self):
+        """Raise AttributeError for missing attribute."""
+
+        test_name = "raise_error"
+        error_msg = f"'AtmosConstants' object has no attribute '{test_name}'"
+
+        with pytest.raises(AttributeError, match=error_msg):
+            test_class = scintillometry.backend.constants.AtmosConstants()
+            test_class.get(test_name)
+
+    @pytest.mark.dependency(
+        name="TestBackendConstants::test_get",
+        depends=["TestBackendConstants::test_get_error"],
+    )
+    def test_get(self):
+        """Get attribute value."""
+
+        test_attribute = ("k", 0.4)  # von Kármán's constant, k=0.4
+        test_class = scintillometry.backend.constants.AtmosConstants()
+        compare_attribute = test_class.get(test_attribute[0])
+
+        assert isinstance(compare_attribute, float)
+        assert compare_attribute == pytest.approx(test_attribute[1])
+
+    @pytest.mark.parametrize("arg_value", [-0.4, 0.5, 1])
+    def test_overwrite(self, arg_value):
+        """Overwrite attribute value."""
+
+        test_class = scintillometry.backend.constants.AtmosConstants()
+        test_class.overwrite("k", arg_value)
+
+        assert isinstance(test_class.k, (float, int))
+        assert test_class.k == pytest.approx(arg_value)
