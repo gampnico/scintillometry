e36fc39c22ded1b2e6af495e87179d8dad7d9bd1
gampnico <45390064+gampnico@users.noreply.github.com>
2023-02-21 17:04:22 +0100

    tests: add fixture to mock derived data
    Add fixture inheriting from fixture_conftest_mock_merged_dataframe,
    which mocks the output from derivation functions.
    
    Refs: ST-3, ST-60

--- tests/conftest.py
+++ tests/conftest.py
@@ -174,7 +174,7 @@ def fixture_conftest_mock_weather_dataframe():
         "wind_direction": [31.0, 235.0, 214.0],
         "vector_wind_speed": [1.2, 0.9, 1.0],
         "wind_speed": [1.2, 0.9, 1.1],
-        "global_radiation": [0.0, 0.0, 0.0],
+        "global_irradiance": [0.1, 23, 56],
         "pressure": [950.5, 950.2, 950.4],
         "relative_humidity": [89.0, 70.0, 79.0],
         "precipitation": [0.0, 0.0, 0.0],
@@ -200,8 +200,8 @@ def fixture_conftest_mock_weather_dataframe_tz(conftest_mock_weather_dataframe):
     yield dataframe
 
 
-@pytest.fixture(scope="function", autouse=False)  # otherwise gets overwritten
-def conftest_mock_merged_dataframe():
+@pytest.fixture(name="conftest_mock_merged_dataframe", scope="function", autouse=False)
+def fixture_conftest_mock_merged_dataframe():
     """Constructs mock dataframe with BLS and weather data."""
 
     data = {
@@ -210,8 +210,11 @@ def conftest_mock_merged_dataframe():
             "2020-06-03T03:20:00Z",
             "2020-06-03T03:30:00Z",
         ],
+        "Cn2": [1.9115e-16, 2.4472e-16, 2.6163e-16],
         "CT2": [1.9343e-04, 2.4764e-04, 2.6475e-04],
+        "H_convection": [4.6, 5.5, 5.5],
         "wind_speed": [1.2, 0.9, 1.1],
+        "pressure": [950.5, 950.2, 950.4],
         "rho_air": [1.166186, 1.166229, 1.166474],
         "temperature_2m": [10.8, 10.7, 10.7],
     }
@@ -220,6 +223,17 @@ def conftest_mock_merged_dataframe():
     dataframe = dataframe.set_index("time")
     dataframe = dataframe.tz_convert("CET")
     assert dataframe.index.name == "time"
+
+    yield dataframe
+
+
+@pytest.fixture(name="conftest_mock_derived_dataframe", scope="function", autouse=False)
+def fixture_conftest_mock_derived_dataframe(conftest_mock_merged_dataframe):
+    """Constructs mock dataframe with parsed InnFLUX data."""
+
+    dataframe = conftest_mock_merged_dataframe.copy(deep=True)
+    dataframe["H_free"] = [28.519535, 118.844056, 155.033711]
+    dataframe["global_irradiance"] = [0.1, 23, 56]
 
     yield dataframe
 
