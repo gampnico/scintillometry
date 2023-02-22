9a621e6d33eec0804820b2a28f84607c31841a78
gampnico <45390064+gampnico@users.noreply.github.com>
2023-02-22 17:02:14 +0100

    feat(metrics): compute fluxes directly from passed datasets
    Adds metrics.calculations module serving as wrapper classes for
    processing input data.
    
    The MetricsWorkflow class provides a template workflow for processing
    wrangled data and user arguments. It will:
    
    - Calculate effective path heights for all stability conditions.
    - Derive |CT2| and SHF for free convection.
    - Estimate the time where stability conditions change.
    - Calculate SHF using MOST.
    - Plot time series comparing SHF with free convection to on-board
      software, time series of SHF calculated with MOST, and a comparison
      to free convection.
    - Save plots to disk.
    
    Refs: ST-3, ST-7, ST-68

---
+++ src/scintillometry/metrics/calculations.py
@@ -0,0 +1,288 @@
+"""Copyright 2023 Nicolas Gampierakis.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=====
+
+Calculates metrics from datasets.
+"""
+
+import pandas as pd
+
+import scintillometry.backend.derivations
+import scintillometry.backend.iterations
+import scintillometry.backend.transects
+import scintillometry.visuals.plotting
+
+
+class MetricsTopography:
+    """Calculate metrics for topographical data."""
+
+    def __init__(self):
+        super().__init__()
+
+    def get_z_params(self, user_args, transect):
+        """Get effective and mean path heights of transect.
+
+        Computes effective and mean path heights of transect under stable
+        and unstable conditions, and with no height dependency. Prints the
+        effective and mean path height for the user-selected stability
+        conditions.
+
+        Select the stability conditions using ``--r, --regime <str>``.
+
+        Args:
+            user_args (argparse.Namespace): Namespace of user arguments.
+            transect (pd.DataFrame): Parsed path transect data.
+
+        Returns:
+            dict[float, float]: Tuples of effective and mean path height,
+            with stability conditions as keys.
+        """
+
+        z_params = scintillometry.backend.transects.get_all_z_parameters(
+            path_transect=transect
+        )
+
+        scintillometry.backend.transects.print_z_parameters(
+            z_eff=z_params[str(user_args.regime)][0],
+            z_mean=z_params[str(user_args.regime)][1],
+            stability=user_args.regime,
+        )
+
+        return z_params
+
+
+class MetricsFlux:
+    """Calculate metrics for fluxes."""
+
+    def __init__(self):
+        super().__init__()
+
+    def construct_flux_dataframe(self, user_args, interpolated_data, z_eff):
+        """Compute sensible heat flux for free convection.
+
+        Computes sensible heat flux for free convection, plots a comparison
+        between the computed flux and the flux recorded by the
+        scintillometer, and saves the figure to disk.
+
+        Warning: this will overwrite existing values for |CT2| in
+        <interpolated_data>.
+
+        Args:
+            user_args (argparse.Namespace): Namespace of user arguments.
+            interpolated_data (pd.DataFrame): Dataframe containing parsed
+                and localised weather and scintillometer data with matching
+                temporal resolution.
+            z_eff (np.floating): Effective path height, [m].
+
+        Returns:
+            pd.DataFrame: Interpolated dataframe with additional column for
+            sensible heat flux under free convection, and overwritten
+            collected |CT2| values with derived values.
+        """
+
+        flux_data = scintillometry.backend.derivations.compute_fluxes(
+            input_data=interpolated_data,
+            effective_height=z_eff[0],
+            beam_params=(user_args.beam_wavelength, user_args.beam_error),
+        )
+
+        return flux_data
+
+    def iterate_fluxes(self, user_args, z_parameters, interpolated_data, most_id):
+        """Compute sensible heat fluxes with MOST through iteration.
+
+        Trades speed from vectorisation for more accurate convergence.
+
+        Args:
+            user_args (argparse.Namespace): Namespace of user arguments.
+            z_parameters (dict[float, float]): Tuples of effective and
+                mean path height, with stability conditions as keys.
+            interpolated_data (pd.DataFrame): Parsed, tz-aware dataframe
+                containing at least |CT2|, wind speed, air density, and
+                temperature.
+
+        Returns:
+            pd.DataFrame: Interpolated data with additional columns for
+            Obukhov length, sensible heat flux, frictional velocity, and
+            temperature scale.
+        """
+
+        # Get time where stability conditions change
+        most_class = scintillometry.backend.iterations.IterationMost()
+
+        switch_time = most_class.get_switch_time(
+            dataframe=interpolated_data, local_time=user_args.switch_time
+        )
+
+        iteration_stable = interpolated_data.iloc[
+            interpolated_data.index.indexer_between_time("00:00", switch_time)
+        ].copy(deep=True)
+        iteration_stable = most_class.most_method(
+            dataframe=iteration_stable,
+            eff_h=z_parameters["stable"][0],
+            stability="stable",
+            coeff_id=most_id,
+        )
+
+        iteration_unstable = interpolated_data.iloc[
+            interpolated_data.index.indexer_between_time(switch_time, "23:59")
+        ].copy(deep=True)
+        iteration_unstable = most_class.most_method(
+            dataframe=iteration_unstable,
+            eff_h=z_parameters["unstable"][0],
+            stability="unstable",
+            coeff_id=most_id,
+        )
+        iteration_data = pd.concat([iteration_stable, iteration_unstable], sort=True)
+
+        return iteration_data
+
+    def plot_derived_metrics(self, user_args, derived_data, time_id, location=""):
+        """Plot and save derived fluxes.
+
+        Args:
+            user_args (argparse.Namespace): Namespace of user arguments.
+            derived_data (pd.DataFrame): Interpolated tz-aware dataframe
+                with column for sensible heat flux under free
+                convection.
+            time_id (pd.TimeStamp): Start time of scintillometer data
+                collection.
+            location (str): Location of data collection. Default empty
+            string.
+
+        Returns:
+            plt.Figure: Time series comparing sensible heat fluxes under
+            free convection to on-board software.
+        """
+
+        figure_convection, _ = scintillometry.visuals.plotting.plot_convection(
+            dataframe=derived_data, stability=user_args.regime, site=location
+        )
+        scintillometry.visuals.plotting.save_figure(
+            figure=figure_convection, timestamp=time_id, suffix="free_convection"
+        )
+
+        return figure_convection
+
+    def plot_iterated_metrics(self, iterated_data, time_stamp, site_location=""):
+        """Plot and save time series and comparison of iterated fluxes.
+
+        Args:
+            user_args (argparse.Namespace): Namespace of user arguments.
+            derived_data (pd.DataFrame): Interpolated tz-aware dataframe
+                with columns for sensible heat fluxes calculated with
+                MOST and for free convection.
+            time_id (pd.TimeStamp): Start time of scintillometer data
+                collection.
+            site_location (str): Name of scintillometer location.
+
+        Returns:
+            tuple[plt.Figure, plt.Figure]: Time series comparing
+            sensible heat fluxes under free convection to on-board
+            software.
+        """
+
+        if iterated_data.attrs:
+            site_name = iterated_data.attrs["name"]
+        else:
+            site_name = site_location
+
+        fig_iter, fig_comp = scintillometry.visuals.plotting.plot_iterated_fluxes(
+            iteration_data=iterated_data, time_id=time_stamp, location=site_name
+        )
+
+        return fig_iter, fig_comp
+
+
+class MetricsWorkflow(MetricsFlux, MetricsTopography):
+    """Standard workflow for metrics."""
+
+    def __init__(self):
+        super().__init__()
+
+    def calculate_standard_metrics(self, arguments, data, site=""):
+        """Calculates and plots metrics from wrangled data.
+
+        This wrapper function:
+
+        - Calculates effective path heights for all stability
+          conditions.
+        - Derives |CT2| and SHF for free convection.
+        - Estimates the time where stability conditions change.
+        - Calculates SHF using MOST.
+        - Plots time series comparing SHF with free convection to
+          on-board software, time series of SHF calculated with MOST,
+          and a comparison to free convection.
+        - Saves plots to disk.
+
+        If this function is imported as a package, mock user arguments
+        with an argparse.Namespace object.
+
+        Args:
+            arguments (argparse.Namespace): User arguments.
+            data (dict): Contains BLS, ZAMG, and transect dataframes, an
+                interpolated dataframe at 60s resolution containing BLS
+                and ZAMG data, and a pd.TimeStamp object of the
+                scintillometer's recorded start time::
+
+                    data = {
+                        "bls": bls_data,
+                        "zamg": zamg_data,
+                        "transect": transect_data,
+                        "interpolated": interpolated_data,
+                        "timestamp": bls_time,
+                        }
+
+            site (str): Location of data collection. Default empty
+                string.
+
+        Returns:
+            dict: Input dictionary with additional keys "derivation",
+            "iteration" for derived and iterated data, respectively.
+        """
+
+        data_timestamp = data["timestamp"]
+        z_params = self.get_z_params(user_args=arguments, transect=data["transect"])
+
+        # Compute free convection
+        derived_dataframe = self.construct_flux_dataframe(
+            user_args=arguments,
+            interpolated_data=data["interpolated"],
+            z_eff=z_params["None"],
+        )
+
+        self.plot_derived_metrics(
+            user_args=arguments,
+            derived_data=derived_dataframe,
+            time_id=data_timestamp,
+            location=site,
+        )
+
+        # Compute fluxes through iteration
+        iterated_dataframe = self.iterate_fluxes(
+            user_args=arguments,
+            z_parameters=z_params,
+            interpolated_data=data["interpolated"],
+            most_id=arguments.most_name,
+        )
+
+        self.plot_iterated_metrics(
+            iterated_data=iterated_dataframe, time_stamp=data_timestamp
+        )
+
+        data["derivation"] = derived_dataframe
+        data["iteration"] = iterated_dataframe
+
+        return data
---
+++ tests/test_metrics_calculations.py
@@ -0,0 +1,331 @@
+"""Copyright 2023 Nicolas Gampierakis.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=====
+
+Tests metrics calculated from datasets.
+
+Only patch mocks for dependencies that have already been tested. When
+patching several mocks via decorators, parameters are applied in the
+opposite order::
+
+    # decorators passed correctly
+    @patch("lib.bar")
+    @patch("lib.foo")
+    def test_foobar(self, foo_mock, bar_mock):
+
+        foo_mock.return_value = 1
+        bar_mock.return_value = 2
+
+        foo_val, bar_val = foobar(...)  # foo_val = 1, bar_val = 2
+
+    # decorators passed in wrong order
+    @patch("lib.foo")
+    @patch("lib.bar")
+    def test_foobar(self, foo_mock, bar_mock):
+
+        foo_mock.return_value = 1
+        bar_mock.return_value = 2
+
+        foo_val, bar_val = foobar(...)  # foo_val = 2, bar_val = 1
+
+"""
+
+import argparse
+
+import matplotlib.pyplot as plt
+import mpmath
+import numpy as np
+import pandas as pd
+import pandas.api.types as ptypes
+import pytest
+
+import scintillometry.metrics.calculations
+
+
+class TestMetricsTopographyClass:
+    """Test class for topography metrics."""
+
+    @pytest.mark.dependency(
+        name="TestMetricsTopographyClass::test_metrics_topography_init"
+    )
+    def test_metrics_topography_init(self):
+        """Boilerplate implementation in case of future changes."""
+
+        test_class = scintillometry.metrics.calculations.MetricsTopography()
+        assert test_class
+
+    @pytest.mark.dependency(
+        name="TestMetricsTopographyClass::test_get_z_params",
+        depends=[
+            "TestBackendTransects::test_get_all_z_parameters",
+            "TestBackendTransects::test_print_z_parameters",
+            "TestMetricsTopographyClass::test_metrics_topography_init",
+        ],
+        scope="session",
+    )
+    @pytest.mark.parametrize("arg_regime", ["stable", "unstable", None])
+    def test_get_z_params(self, capsys, conftest_mock_transect_dataframe, arg_regime):
+        """Get effective and mean path heights of transect."""
+
+        test_metrics = scintillometry.metrics.calculations.MetricsTopography()
+        test_args = argparse.Namespace(regime=arg_regime)
+        assert isinstance(test_args, argparse.Namespace)
+        assert test_args.regime == arg_regime
+
+        compare_metrics = test_metrics.get_z_params(
+            user_args=test_args, transect=conftest_mock_transect_dataframe
+        )
+
+        assert isinstance(compare_metrics, dict)
+        for key in ["stable", "unstable", "None"]:
+            assert key in compare_metrics
+        for values in compare_metrics.values():
+            assert isinstance(values, tuple)
+            assert len(values) == 2
+            assert all(isinstance(z, float) for z in values)
+        for key in ["stable", "unstable"]:
+            assert compare_metrics[key][0] < compare_metrics[key][1]
+        assert compare_metrics["None"][0] > compare_metrics["None"][1]
+
+        compare_print = capsys.readouterr()
+        if not arg_regime:
+            assert "Selected no height dependency:" in compare_print.out
+        else:
+            assert str(arg_regime) in compare_print.out
+        assert (
+            str(f"Effective path height:\t{compare_metrics[str(arg_regime)][0]:>0.2f}")
+            in compare_print.out
+        )
+        assert (
+            str(f"Mean path height:\t{compare_metrics[str(arg_regime)][1]:>0.2f}")
+            in compare_print.out
+        )
+
+
+class TestMetricsFluxClass:
+    """Test class for flux metrics."""
+
+    @pytest.mark.dependency(name="TestMetricsFluxClass::test_metrics_flux_init")
+    def test_metrics_flux_init(self):
+        """Boilerplate implementation in case of future changes."""
+
+        test_class = scintillometry.metrics.calculations.MetricsFlux()
+        assert test_class
+
+    @pytest.mark.dependency(
+        name="TestMetricsFluxClass::test_construct_flux_dataframe",
+        depends=["TestBackendDerivations::test_compute_fluxes"],
+        scope="session",
+    )
+    @pytest.mark.parametrize("arg_regime", ["stable", "unstable", None])
+    def test_construct_flux_dataframe(self, conftest_mock_merged_dataframe, arg_regime):
+        """Compute sensible heat flux for free convection."""
+
+        test_metrics = scintillometry.metrics.calculations.MetricsFlux()
+
+        test_args = argparse.Namespace(
+            regime=arg_regime, beam_wavelength=880, beam_error=20
+        )
+        test_frame = conftest_mock_merged_dataframe[["CT2", "H_convection"]]
+
+        compare_metrics = test_metrics.construct_flux_dataframe(
+            user_args=test_args,
+            interpolated_data=conftest_mock_merged_dataframe,
+            z_eff=(100, 200),
+        )
+        for key in ["CT2", "H_free", "H_convection"]:
+            assert key in compare_metrics.columns
+            assert ptypes.is_numeric_dtype(compare_metrics[key])
+        assert not np.allclose(compare_metrics["CT2"], test_frame["CT2"])
+        assert np.allclose(compare_metrics["H_convection"], test_frame["H_convection"])
+
+    @pytest.mark.dependency(name="TestMetricsFluxClass::test_plot_derived_metrics")
+    @pytest.mark.parametrize("arg_regime", ["stable", "unstable", None])
+    def test_plot_derived_metrics(
+        self, conftest_mock_save_figure, conftest_mock_derived_dataframe, arg_regime
+    ):
+        """Plot time series of heat fluxes for free convection."""
+
+        _ = conftest_mock_save_figure
+        test_metrics = scintillometry.metrics.calculations.MetricsFlux()
+        test_frame = conftest_mock_derived_dataframe
+        test_args = test_args = argparse.Namespace(
+            regime=arg_regime, beam_wavelength=880, beam_error=20
+        )
+        compare_fig = test_metrics.plot_derived_metrics(
+            user_args=test_args, derived_data=test_frame, time_id=test_frame.index[0]
+        )
+        assert isinstance(compare_fig, plt.Figure)
+
+        compare_ax = plt.gca()
+        if arg_regime:
+            compare_conditions = f"{arg_regime.capitalize()} Conditions"
+        else:
+            compare_conditions = "No Height Dependency"
+        compare_title = (
+            "Sensible Heat Fluxes from On-Board Software and",
+            f"for Free Convection ({compare_conditions}), 03 June 2020",
+        )
+        assert compare_ax.get_title() == " ".join(compare_title)
+        plt.close("all")
+
+    @pytest.mark.dependency(name="TestMetricsFluxClass::test_plot_iterated_metrics")
+    @pytest.mark.parametrize("arg_location", [None, "", "Test Location"])
+    def test_plot_iterated_metrics(
+        self, conftest_mock_save_figure, conftest_mock_iterated_dataframe, arg_location
+    ):
+        """Plot time series of iteration and comparison to free convection."""
+
+        _ = conftest_mock_save_figure
+        test_metrics = scintillometry.metrics.calculations.MetricsFlux()
+        test_frame = conftest_mock_iterated_dataframe.copy(deep=True)
+        test_stamp = conftest_mock_iterated_dataframe.index[0]
+        if arg_location:
+            test_frame.attrs["name"] = arg_location
+            assert "name" in test_frame.attrs
+
+        compare_iter, compare_comp = test_metrics.plot_iterated_metrics(
+            iterated_data=test_frame,
+            time_stamp=test_stamp,
+            site_location=arg_location,
+        )
+
+        for fig in [compare_iter, compare_comp]:
+            assert isinstance(fig, plt.Figure)
+
+        if arg_location:
+            test_location = f" at {arg_location}"
+        else:
+            test_location = ""
+        test_iter_title = (
+            "Sensible Heat Flux",
+            f"{test_location}, 03 June 2020",
+        )
+        assert compare_iter.gca().get_title() == "".join(test_iter_title)
+
+        test_comp_title = (
+            "Sensible Heat Flux from Free Convection and Iterated Flux",
+            f"{test_location}, 03 June 2020",
+        )
+        assert compare_comp.gca().get_title() == "".join(test_comp_title)
+
+        plt.close("all")
+
+    @pytest.mark.dependency(name="TestMetricsFluxClass::test_iterate_fluxes")
+    def test_iterate_fluxes(
+        self,
+        conftest_mock_save_figure,
+        conftest_mock_derived_dataframe,
+    ):
+        """Compute sensible heat fluxes with MOST through iteration."""
+
+        _ = conftest_mock_save_figure  # otherwise figure is saved to disk
+        test_metrics = scintillometry.metrics.calculations.MetricsFlux()
+
+        test_args = argparse.Namespace(
+            switch_time="05:24", beam_wavelength=880, beam_error=20
+        )
+
+        compare_metrics = test_metrics.iterate_fluxes(
+            user_args=test_args,
+            z_parameters={"stable": (100, 150), "unstable": (2, 150)},
+            interpolated_data=conftest_mock_derived_dataframe,
+            most_id="an1988",
+        )
+        compare_keys = [
+            "u_star",
+            "theta_star",
+            "f_ct2",
+            "shf",
+            "obukhov",
+        ]
+        assert isinstance(compare_metrics, pd.DataFrame)
+        for key in compare_keys:
+            assert not (compare_metrics[key].isnull()).any()
+            assert key in compare_metrics.keys()
+            assert all(isinstance(x, (mpmath.mpf)) for x in compare_metrics[key])
+        plt.close("all")
+
+
+class TestMetricsWorkflowClass:
+    """Test class for metrics workflow.
+
+    Effectively serves as integration test for module.
+    """
+
+    @pytest.mark.dependency(name="TestMetricsWorkflowClass::test_metrics_workflow_init")
+    def test_metrics_workflow_init(self):
+        """Boilerplate implementation in case of future changes."""
+
+        test_class = scintillometry.metrics.calculations.MetricsWorkflow()
+        assert test_class
+
+    @pytest.mark.dependency(
+        name="TestMetricsWorkflow::test_calculate_standard_metrics",
+        depends=[
+            "TestMetricsWorkflowClass::test_metrics_workflow_init",
+            "TestMetricsTopographyClass::test_get_z_params",
+            "TestMetricsFluxClass::test_construct_flux_dataframe",
+            "TestMetricsFluxClass::test_plot_derived_metrics",
+            "TestMetricsFluxClass::test_iterate_fluxes",
+            "TestMetricsFluxClass::test_plot_iterated_metrics",
+        ],
+        scope="module",
+    )
+    @pytest.mark.parametrize("arg_regime", [None, "stable", "unstable"])
+    @pytest.mark.parametrize("arg_switch_time", [None, "05:24"])
+    @pytest.mark.parametrize("arg_most_name", ["an1988", "li2012"])
+    @pytest.mark.parametrize("arg_location", [None, "", "Test Location"])
+    def test_calculate_standard_metrics(
+        self,
+        conftest_mock_save_figure,
+        conftest_mock_bls_dataframe_tz,
+        conftest_mock_weather_dataframe_tz,
+        conftest_mock_transect_dataframe,
+        conftest_mock_merged_dataframe,
+        arg_regime,
+        arg_switch_time,
+        arg_most_name,
+        arg_location,
+    ):
+        """Calculate and plot standard metrics."""
+
+        _ = conftest_mock_save_figure
+        test_class = scintillometry.metrics.calculations.MetricsWorkflow()
+        test_data = {
+            "bls": conftest_mock_bls_dataframe_tz.copy(deep=True),
+            "weather": conftest_mock_weather_dataframe_tz.copy(deep=True),
+            "transect": conftest_mock_transect_dataframe.copy(deep=True),
+            "interpolated": conftest_mock_merged_dataframe.copy(deep=True),
+            "timestamp": conftest_mock_bls_dataframe_tz.index[0],
+        }
+
+        test_args = argparse.Namespace(
+            regime=arg_regime,
+            switch_time=arg_switch_time,
+            beam_wavelength=880,
+            beam_error=20,
+            most_name=arg_most_name,
+        )
+
+        compare_data = test_class.calculate_standard_metrics(
+            arguments=test_args, data=test_data, site=arg_location
+        )
+        plt.close("all")
+
+        assert isinstance(compare_data, dict)
+        for key in ["derivation", "iteration"]:
+            assert key in compare_data
+            assert isinstance(compare_data[key], pd.DataFrame)
