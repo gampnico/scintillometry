c32a030df520a1c82d4154870ad4c18bde9dfd70
gampnico <45390064+gampnico@users.noreply.github.com>
2023-05-16 12:25:29 +0200

    docs: fix rtd configuration
    Corrects package installation path.
    
    Refs: ST-2, ST-122

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,7 +49,7 @@ def setup(app):
 project = "Scintillometry"
 copyright = f"2019-{date.today().year}, Scintillometry Contributors"
 author = "Scintillometry Contributors"
-release = "1.0.2"
+release = "1.0.3"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
